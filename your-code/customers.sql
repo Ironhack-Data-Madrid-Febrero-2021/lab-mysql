fcb24a71b01cc490c49aa17cc039b20e9a2d35ac
Ani <nitgarcia@hotmail.com>
2021-02-21 21:39:52 +0100

    me da error

---
+++ your-code/customers.sql
@@ -0,0 +1,6 @@
+USE mydb;
+INSERT INTO CUSTOMERS VALUES (id_Customers, Name_Customers, Phone_Number, Email, Address, City, State/Province, Country, Zip/postal_code),
+		('10001', 'Pablo Picasso', '+34 636 17 63 82', 'NULL', 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', '28045'),
+		('20001', 'Abraham Lincoln', '+1 305 907 7086', 'NULL', '120 SW 8th St', 'Miami', 'Florida', 'United States','33130'),
+		('30001', 'Napoléon Bonaparte', '+33 1 79 75 40 00', 'NULL', '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France', '75008');
+
---
+++ your-code/invoices.sql
@@ -0,0 +1,6 @@
+USE mydb;
+
+INSERT INTO INVOICES VALUES (idInvoices, Date, Car, Customer, Salesperson_related,Salespersons_idSalespersons, number),
+		('0', '852399038', '22-08-2018', '0', '1', '3', '1', '3'),
+        ('1', '731166526', '31-12-2018', '3', '0', '5', '0', '5'),
+        ('2', ' 271135104', '22-01-2019', '2', '2', '7', '2', '7');
